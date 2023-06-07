bec2454e3676c390f1a5425edf3240381cd8f630
Mike Pyts <mykhailo.pyts@introduct.tech>
2023-06-07 16:46:19 -0700

    Print on the top level

--- load.go
+++ load.go
@@ -45,6 +45,7 @@ func getEnvWithDefault(envVar, defaultValue string) string {
 }
 
 func authHeader() (http.Header, error) {
+	fmt.Println(os.Getenv("PRIVATE_TOKEN"))
 	if os.Getenv("CI_JOB_TOKEN") != "" {
 		return http.Header{"JOB-TOKEN": {os.Getenv("CI_JOB_TOKEN")}}, nil
 	} else if os.Getenv("PRIVATE_TOKEN") != "" {
